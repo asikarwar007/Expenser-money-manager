885a3811f1938ac9c40131201e4198c2d7ded62b
asikarwar <asikarwar007@gmail.com>
2017-10-19 13:05:27 +0530

    UI get item

--- app.js
+++ app.js
@@ -3,11 +3,45 @@ var budgetController =(function () {
 })();
 
 var UIController = (function () {
+  var DOMstring = {
+    inputType : '.add__type',
+    addDescription : '.add__description',
+    addValue : '.add__value',
+    addButtun : '.add__btn',
 
+
+  };
+  return {
+    getInput : function () {
+      return {
+         type : document.querySelector(DOMstring.inputType).value,
+         description : document.querySelector(DOMstring.addDescription).value,
+         value : document.querySelector(DOMstring.addValue).value
+      };
+    },
+    getDOMstrings : function () {
+        return DOMstring;
+    }
+
+
+  };
 
 })();
 
 var controller = (function (budgetCtrl,UICtrl) {
+   var DOM = UICtrl.getDOMstrings();
+  var controllerAddItem = function() {
 
+    var input = UICtrl.getInput();
+    console.log(input);
+  };
+
+  document.querySelector(DOM.addButtun).addEventListener('click',controllerAddItem);
+  document.addEventListener('Keypress',function(event) {
+    if(event.keycode === 13 || event.which === 13){
+      controllerAddItem();
+
+    }
+  });
 
 })(budgetController, UIController);
